b7d9e094cabffe9e3dfc4a1e459f1424c8d971e2
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2019-04-04 12:06:07 +0100

    Added more interface
    Added more functions to Text Interface class to increase play ability.

--- Monopoly/TextInterface.py
+++ Monopoly/TextInterface.py
@@ -5,7 +5,7 @@ File thats holds the class which controlls all text inputs for the monopoly game
 class TextInterface:
 
     def showWelcome(self):
-        print("Welcome to Monopoly\nThis game took me FUCKING ages to make so please be kind")
+        print("Welcome to Monopoly\nThis game took me FUCKING ages to make so please be kind\nYou start with £500")
 
     def getNumPlayers(self):
         numPlayers = int(input("How many Players?"))
@@ -19,3 +19,30 @@ class TextInterface:
     
     def showDetails(self, player):
         print("{} has £{}\nProperties: {}\nPosition: {}".format(player.name, player.money, player.properties, player.position))
+    
+    def wantToBuy(self, propertyGiven):
+        choice = input("Do You wish to buy {} for £{}? Y/N".format(propertyGiven.name, propertyGiven.price)).lower()
+        if choice == "y":
+            return True
+        else:
+            return False
+    
+    def success(self):
+        print("Success!")
+
+    def fail(self):
+        print("Sorry You Cant Do that!")
+    
+    def rollDice(self):
+        input("Roll Dice...")
+
+    def rollAgain(self):
+        input("Double! Roll Again")
+    
+    def showDice(self, result):
+        print("You rolled a {} and a {}!".format(result[0], result[1]))
+        input("Continue...")
+    
+    def startTurn(self, player):
+        print("It is {} Turn".format(player.name))
+        input("Continue...")
