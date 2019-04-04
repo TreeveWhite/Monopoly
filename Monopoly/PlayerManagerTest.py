4ad6700647e1d8c8876a7a33ae507964f7bb3674
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2019-04-04 10:44:55 +0100

    Added Unit test for getplayers
    Added template for Louis as well.

--- Monopoly/PlayerManagerTest.py
+++ Monopoly/PlayerManagerTest.py
@@ -11,6 +11,13 @@ def testPlayerBuy(playermanagement, interface):
 
     assert player.properties[0].name == "Mediterranean Avenue", "Buying does not work"
 
+def testGetPlayers(playermanagement, interface):
+    numPlayers = 2
+    print(playermanagement.getPlayers(numPlayers, interface))
+
+def testProotertyPlayeron(playermanagement, interface):
+    #Unit test the func propteryp player on ie make a player object and pass into fuc check it gives right pos.
+
 if __name__ == "__main__":
     playermanagement = PlayerManager()
     interface = TextInterface()
