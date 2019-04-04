278796439ff556e4ec24d6467d0a9b3fc7ec214c
Reece N <33102368+magicrwn@users.noreply.github.com>
2019-04-04 11:25:26 +0100

    playerBuy added
    Updates property owned by, balance, and adds to players property collection.

---
+++ PlayerManager.py
@@ -0,0 +1,60 @@
+"""
+File that holds the Playermanager Class.
+"""
+
+from Player import *
+from Dice import *
+from SquareClasses import *
+
+class PlayerManager:
+
+    def getPlayers(self, numPlayers, inputInterface):
+        players = []
+        names = inputInterface.getPlayerNames(numPlayers)
+        for player in range(numPlayers):
+            name = names[player]
+            p = Player(name, 500, [], 0)
+            players.append(p)
+        return players
+
+    def propertyPlayerOn(self, position, board):
+        for place in board:
+            if place.position == position:
+                return place
+
+    def takeTurn(self, player, board):
+        distance = 0
+        goPrision = False
+        for roll in range(3):
+            result = Dice.roll()
+            distance += result[0] + result[1]
+            if result[0] == result[1] and roll == 2:
+                goPrision = True
+            elif result[0] == result[1]:
+                continue
+            else:
+                break   
+                
+        if goPrision:
+            #send player to prison.
+            player.movePlayer(100)
+        else:
+            player.movePlayer(distance)
+            propertyOn = self.propertyPlayerOn(player.position, board)
+
+
+    def playerBuy(self, player, board):
+        propertyPositionToBuy = player.position
+        propertyClass = self.propertyPlayerOn(propertyPositionToBuy, board)
+        """
+        print(propertyPositionToBuy)
+        print(propertyClass.name)"""
+
+        if player.money >= propertyClass.price:
+            propertyClass.ownedBy = player
+            player.money -= propertyClass.price
+            player.properties.append(propertyClass)
+            #propertyClass.prettyPrint()
+            return True
+        else:
+            return False
