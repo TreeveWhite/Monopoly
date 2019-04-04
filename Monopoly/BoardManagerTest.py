1900ea5c19598823e5b5049bcd14d78a944a81b5
Reece N <33102368+magicrwn@users.noreply.github.com>
2019-04-04 12:02:24 +0100

    Added new tests
    Tests that it loads the correct file and that the board names and positions are as expected.

--- Monopoly/BoardManagerTest.py
+++ Monopoly/BoardManagerTest.py
@@ -1,7 +1,41 @@
 from BoardManager import *
 
+
+def testGenerateBoardFirstSquareName():
+	b = BoardManager()
+	board = b.generateBoard()
+
+	assert board[0].name == "Go", "Expected 'Go' square name."
+
+def testGenerateBoardFirstSquarePosition():
+	b = BoardManager()
+	board = b.generateBoard()
+
+	assert board[0].position == 0, "Expected 'Go' square postion."
+	
+
+def testGenerateBoardTenthSquareName():
+	b = BoardManager()
+	board = b.generateBoard()
+
+	assert board[9].name == "Bow Street", "Expected 'Bow Street' square."
+
+def testGenerateBoardTenthSquarePrice():
+	b = BoardManager()
+	board = b.generateBoard()
+
+	assert board[9].price == 160, "Expected 'Bow Street' square price to be 160"
+
+
+
+
 if __name__ == "__main__":
 
-	b = BoardManager()
-	b.generateBoard()
 	print("It passed...") # it didn't :(
+	testGenerateBoardFirstSquareName()
+	testGenerateBoardFirstSquarePosition()
+
+	testGenerateBoardTenthSquareName()
+	testGenerateBoardTenthSquarePrice()
+
+	testPrintBoard()
