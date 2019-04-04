b66145e5fb003529fa073691b69b5c140b1986b3
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2019-04-04 10:35:35 +0100

    Added unit test for PlayerBuy Func
    Added a unit test for player buy function inside of the Player management class.

--- Monopoly/PlayerManagerTest.py
+++ Monopoly/PlayerManagerTest.py
@@ -1,3 +1,14 @@
+"""
+Unit Tests for PlayerManager
+"""
 
-#hi
+from PlayerManager import *
 
+def testPlayerBuy(playermanagement, interface):
+    numPlayers = 2
+    player = Player("p1", 500, [], 1)
+    playermanagement.playerBuy(player)
+
+    assert player.properties[0].name == "Mediterranean Avenue", "Buying does not work"
+
+if __name__ == "__main__":
