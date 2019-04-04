437b5e9898e11ea6415ae6bab0f9b0963a070900
Reece N <33102368+magicrwn@users.noreply.github.com>
2019-04-04 11:24:26 +0100

    Test for player buying property added
    Yay!

--- Monopoly/PlayerManagerTest.py
+++ Monopoly/PlayerManagerTest.py
@@ -4,21 +4,19 @@ Unit Tests for PlayerManager
 
 from PlayerManager import *
 from TextInterface import *
+from BoardManager import *
 
-def testPlayerBuy(playermanagement, interface):
+def testPlayerBuy(playermanagement, interface, board):
     player = Player("p1", 500, [], 1)
-    playermanagement.playerBuy(player, board)
+    didByWork = playermanagement.playerBuy(player, board)
 
     assert player.properties[0].name == "Mediterranean Avenue", "Buying does not work"
-
-def testGetPlayers(playermanagement, interface):
-    numPlayers = 2
-    print(playermanagement.getPlayers(numPlayers, interface))
-
-def testProotertyPlayeron(playermanagement, interface):
-    #Unit test the func propteryp player on ie make a player object and pass into fuc check it gives right pos.
+    assert didByWork == True, "Buying - was expecting true, got false"
 
 if __name__ == "__main__":
-    playermanagement = PlayerManager()
+    playerManagement = PlayerManager()
     interface = TextInterface()
-    testPlayerBuy(playermanagement, interface)
+    boardManager = BoardManager()
+    board = boardManager.generateBoard()
+    
+    testPlayerBuy(playerManagement, interface, board)
