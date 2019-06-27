70517f41fdfbab68837fbf8e27afe0e7156f2e2d
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2019-06-27 12:05:52 +0100

    Made a fair dice again.

--- Monopoly/Dice.py
+++ Monopoly/Dice.py
@@ -7,6 +7,6 @@ import random
 class Dice:
 
     def roll():
-        die1 = random.randint(1, 1)
-        die2 = random.randint(2, 2)
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
         return [die1, die2]
